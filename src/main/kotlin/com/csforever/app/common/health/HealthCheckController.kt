08469747f7059b40ace8e1d33289bbe779a833c2
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-05-14 22:09:00 +0900

    chore : Docker Setting

--- src/main/kotlin/com/csforever/app/common/health/HealthCheckController.kt
+++ src/main/kotlin/com/csforever/app/common/health/HealthCheckController.kt
@@ -1,15 +1,19 @@
 package com.csforever.app.common.health
 
 import io.swagger.v3.oas.annotations.Hidden
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController {
 
+    private var logger = LoggerFactory.getLogger(HealthCheckController::class.java)
+
     @Hidden
     @GetMapping("/")
     suspend fun healthCheck(): String {
+        logger.info("HEALTH CHECK")
         return "OK"
     }
 }
