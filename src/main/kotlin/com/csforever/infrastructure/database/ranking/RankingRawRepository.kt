93fa5a866bcab951182e5fe1a50df09c6daa8982
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-05-29 19:57:45 +0900

    fix : 통계 측정 스크립트 수정

--- src/main/kotlin/com/csforever/infrastructure/database/ranking/RankingRawRepository.kt
+++ src/main/kotlin/com/csforever/infrastructure/database/ranking/RankingRawRepository.kt
@@ -11,20 +11,27 @@ class RankingRawRepository(
 
     suspend fun analyzeRanking() {
         val sql = """
-            INSERT INTO user_ranking (user_id, ranking, type, correct_submission_count, updated_at)
+            INSERT INTO user_ranking (
+                user_id,
+                ranking,
+                type,
+                correct_submission_count,
+                updated_at
+            )
             SELECT 
                 user_id,
                 RANK() OVER (ORDER BY COUNT(DISTINCT question_id) DESC),
                 'TOTAL',
                 COUNT(DISTINCT question_id),
-                NOW()
-            FROM Submission
+                CONVERT_TZ(NOW(), 'UTC', 'Asia/Seoul')
+            FROM submission
             WHERE is_correct = 1
+              AND user_id != 0
             GROUP BY user_id
             ON DUPLICATE KEY UPDATE 
                 ranking = VALUES(ranking),
                 correct_submission_count = VALUES(correct_submission_count),
-                updated_at = VALUES(updated_at)
+                updated_at = VALUES(updated_at);
         """.trimIndent()
 
         databaseClient.sql(sql)
